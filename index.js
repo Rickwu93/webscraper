8e82cae917d04e9adff22d0e6f6ad086c9d30500
Ricky Wu <rickywu@Rickys-MacBook-Air.local>
2022-08-10 02:36:34 -0600

    basic sneaker scrape for title and url link

--- index.js
+++ index.js
@@ -5,4 +5,25 @@ const express = require('express')
 
 const app = express();
 
+const url = 'https://www.nike.com/ca/w/new-3n82y'
+
+axios(url)
+    .then (response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const list = []
+
+        $('.product-card', html).each(function() {
+        const title = $(this).text()
+        const url = $(this).find('a').attr('href')
+        list.push({
+            title,
+            url
+           })
+        })
+        console.log(list)
+    }).catch(err => console.log(err))
+
+
+
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
